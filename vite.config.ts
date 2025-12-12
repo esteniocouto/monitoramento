2aa4bd3684f29adedb73949df464512644917ca5
ESTENIO COUTO BORGES JUNIOR <117777914+esteniocouto@users.noreply.github.com>
2025-12-12 10:39:29 -0300

    feat: Add system startup script and Vite config
    Introduces a new batch script to automate the startup of both the backend and frontend applications. Also, updates the Vite configuration to explicitly define the server port and host.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    strictPort: true, // Garante que falhe se a porta estiver ocupada, em vez de mudar para 5174
+    strictPort: true, // Garante que não mude para 5174 se a porta estiver ocupada
     host: true
-  }
-});
+  },
+})
